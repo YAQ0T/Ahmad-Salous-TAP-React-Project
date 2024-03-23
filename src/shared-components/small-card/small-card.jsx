5bc07aa92a88e77f29780760f6c27175ad4cb565
YAQOT <63197328+YAQ0T@users.noreply.github.com>
2024-03-23 16:11:05 +0200

    fix images in details

--- src/shared-components/small-card/small-card.jsx
+++ src/shared-components/small-card/small-card.jsx
@@ -4,7 +4,7 @@ import { Stars } from "../stars/stars";
 export function SmallCard({ image, name, stars }) {
   return (
     <div className={styles.smallCard}>
-      <img src={`/logos/${image}`} alt={image} />
+      <img src={require(`../../assits/logos/${image}`)} alt={image} />
       <div className={styles.smallCardInfo}>
         <Paragraph size={"16px"} weight={"800"} data={name} />
         <Stars numberOfStars={stars} />
